a217f824c1feeef02727fdb1bf4c719dbdd4e538
YassineKSYAR <99976091+YassineKSYAR@users.noreply.github.com>
2022-05-11 14:42:44 +0200

    Update kill.c

--- src/kill.c
+++ src/kill.c
@@ -7,22 +7,69 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-main( int argc,char *argv[] )
+int main (int argc, char* argv[])
 {
-    int i,fd1,fd2;
-    char *file1,*file2,buf[2];
-    
-    file1 = argv[1];
-    file2 = argv[2];
+    if (argc <=1) {
+        DIR *dp;
+        struct dirent *dirp;
+        char path [100];
+        getcwd(path,100);
 
-    fd1 = open(file1,O_RDONLY,0777);
-    fd2 = creat(file2,0777);
+        argv[1] = path;
+        if ((dp = opendir(argv[1])) == NULL) {
+             perror("ERROR");
+        }
 
-    while(i = read(fd1,buf,1) > 0)
-    write(fd2,buf,1);
-    
-    remove(file1);
+        while ((dirp = readdir(dp)) != NULL)
+            if(dirp -> d_type == DT_REG){
 
-    close(fd1);
-    close(fd2);
+                if(strstr(dirp->d_name,"char-")){ //get characters only
+                    char character[30];
+                    strcpy(character,dirp->d_name);
+                    char *last = strchr(character,'-');
+                    char charname[15];
+                    int i = 1;
+                    while(last[i]!='.'){
+                        charname[i-1] = last[i];
+                        i++;
+                    }
+
+                    printf("\n%s\n\n", charname);
+                    memset(charname, 0, sizeof charname); //reset
+
+                }
+            }
+
+        if (closedir(dp) == -1)
+            perror("closedir");
+    }
+
+    else if(argc==2){
+        char character[30];
+        strcat(character, argv[1]);
+
+        int fd = open(character, 0_RDONLY);
+        if(fd<0){
+            puts("Calm down! You're a man of justice, you can't kill anyone!");
+            exit(0);
+        }
+
+        char buf;
+        int vic,n;
+        char deadchar[30];
+
+        strcpy(deadchar, "dead-");
+        strcat(deadchar, argv[1]);
+
+        vic=open(argv[1],O_RDONLY);  // Open the File
+        while((n=read(vic,&buf,1)) != 0){
+            rename(charname, deadchar);
+            // Initilize the killed character name by "dead-"
+        }
+        printf("Character killed Successfully\n");
+        close(vic);
+    }
+return 0;
 }
+
+
